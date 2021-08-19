64520ededb4a339267efcb748684b136f707f4d2
Siemensbot <89146090+Siemensbot@users.noreply.github.com>
2021-08-19 17:40:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'SIEMENSBOT🤖'; 
+const instagram = 'https://instagram.com/siemenswap'; 
+const telegram = 'https://t.me/'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://www.youtube.com/c/siemenswap';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
@@ -651,7 +651,7 @@ if (text.includes("!alay")){
 
 
 
-//AKHIRNYAAAAAAAA
+/Habisss
 
 
 })
